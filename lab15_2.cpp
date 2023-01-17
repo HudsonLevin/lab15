dbd8e74c6c132a1e65fab2aa5b51b49c9a7acd2c
Hudson Levine <gamem45000@gmail.com>
2023-01-17 17:13:54 +0700

    lab15_2.cpp completed

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -19,3 +19,35 @@ int main(){
 	showMatrix(B);
 	return 0;
 }
+
+void showMatrix(const bool g[][N]){
+    for(int i=0;i<N;i++){
+        for(int j=0;j<N;j++){
+            cout<<g[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+void inputMatrix(double g[][N]){
+    for(int i=0;i<N;i++){
+    cout<<"Row "<<i+1<<": ";
+        for(int j=0;j<N;j++){
+            cin >> g[i][j];
+        }
+    }
+}
+
+void findLocalMax(const double g[][N], bool d2[][N]){
+    for(int i = 0; i < N; i++){
+		for(int j = 0; j < N; j++){
+			if ((i > 0 && i <= N - 2) && (j > 0 && j <= N - 2)){
+				if ((g[i][j] >= g[i - 1][j]) && (g[i][j] >= g[i][j - 1]) && (g[i][j] >= g[i][j + 1]) && (g[i][j] >= g[i + 1][j])){
+					d2[i][j] = 1;
+				}
+				else d2[i][j] = 0;
+			}
+			else d2[i][j] = 0;
+		}
+	}
+}
